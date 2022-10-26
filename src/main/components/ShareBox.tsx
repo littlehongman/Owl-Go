d263fab3553b43b8999343cf065971e60c6f4035
Johnson <hungjiewu@gmail.com>
2022-10-26 01:06:05 -0500

    feat: add ClearBtn

--- src/main/components/ShareBox.tsx
+++ src/main/components/ShareBox.tsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { useFilePicker } from "use-file-picker";
 import { addPost } from "../../store/actions";
 import { Post } from "../../store/types";
+import { MdOutlineClear } from "react-icons/md"
 
 
 interface UserProps {
@@ -23,9 +24,9 @@ const ShareBox = ({userId, setNewPost}: UserProps) => {
         accept: ['.png', '.jpg'],
     });
 
-    const handleClick = (e: React.FormEvent<HTMLFormElement>):void => {
+    const handleClick = ():void => {
         console.log(filesContent);
-        e.preventDefault();
+        // e.preventDefault();
         
         if (message.trim() !== "" && userId){
             //let newPosts = [...userPosts?? []]
@@ -47,6 +48,7 @@ const ShareBox = ({userId, setNewPost}: UserProps) => {
 
             //setUserPosts([post, ...userPosts]);
             dispatch(addPost(post));
+            setMessage("")
             setNewPost(message);
             toast.success("Successful Post", {duration: 1000})
         }
@@ -59,20 +61,23 @@ const ShareBox = ({userId, setNewPost}: UserProps) => {
 
     return (
         <div className="container mx-auto rounded-xl shadow border m-4 bg-white">
-            <form onSubmit={handleClick}>
+            {/* <form onSubmit={handleClick}> */}
                 <label htmlFor="chat" className="sr-only">Your message</label>
                 <div className="flex items-center py-2 px-3 bg-gray-50 rounded-lg dark:bg-gray-700">
                     <button type="button" onClick={() => openFileSelector()}  className="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
                         <svg aria-hidden="true" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd"></path></svg>
                         <span className="sr-only">Upload image</span>
                     </button>
-                    <textarea id="chat" rows={1} onChange={(e) => setMessage(e.target.value)} className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="What you have in mind..."></textarea>
-                    <button type="submit" className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
+                    <textarea id="chat" rows={1} value={message} onChange={(e) => setMessage(e.target.value)} className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="What you have in mind..."></textarea>
+                    <button type="submit" onClick={() => handleClick()}className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
                         <svg aria-hidden="true" className="w-6 h-6 rotate-90" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg>
                         <span className="sr-only">Send message</span>
                     </button>
+                    <button onClick={() => setMessage("")} className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
+                        <MdOutlineClear className="w-6 h-6"/>
+                    </button>
                 </div>
-            </form>
+            {/* </form> */}
         </div>
     )
 
